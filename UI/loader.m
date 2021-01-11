2cec1edfcb330c53bfbf1175259fb55d996a3ec8
dohaison <dohaison1998@gmail.com>
2021-01-11 14:27:07 +0700

    fix_loader

--- UI/loader.m
+++ UI/loader.m
@@ -2,13 +2,13 @@ function loader
     f = waitbar(0,'Please wait...');
     pause(.1)
     
-    main();
-
     waitbar(.33,f,'Opening the application');
     pause(.1)
-
+    
     waitbar(.67,f,'Opening the application');
     pause(.1)
+    
+    main();
 
     waitbar(1,f,'Opening the application');
     pause(.1)
