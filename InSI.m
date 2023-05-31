9d2abba04844d0b7fa78dd2de9d851352c36105f
DoHaiSon <dohaison1998@gmail.com>
2023-06-01 00:26:58 +0700

    move close_InSI after addpath

--- InSI.m
+++ InSI.m
@@ -7,16 +7,16 @@ function InSI()
 % Author: Do Hai Son - AVITECH - VNU UET - VIETNAM
 % Last Modified by Son 31-May-2023 17:52:13 
 
-%% Close all InSI windows
-if ~close_InSI()
-    return
-end
-
 global main_path;
 main_path = mfilename('fullpath'); %   get path of active file
 main_path = main_path(1:end-4);
 addpath(fullfile(main_path, 'Shared', 'Utils'));
 addpath(genpath_exclude(main_path, {'.git'}));
+
+%% Close all InSI windows
+if ~close_InSI()
+    return
+end
 
 % Clear auto save file of matlab
 clear_asv_files(main_path);
