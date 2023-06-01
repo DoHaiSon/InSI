2ccb1d4999eee697f6785e250e64817e0db0f379
linhtrungvnu <linhtrung.vnu@gmail.com>
2023-06-01 17:07:25 +0700

    test

---
+++ README_Trung.m
@@ -0,0 +1,9 @@
+Cap nhat latest commit: git pull origin master
+
+Check status da modified: git status
+
+Them cac sua doi vao commit moi: git add .
+
+Commit local: git commit -m "msg"
+
+Cap nhat len github.com: git push origin master
